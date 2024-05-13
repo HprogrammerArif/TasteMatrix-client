1f2f33095e58bc10128511e34f1144157190b25d
HprogrammerArif <programmerhabluarif@gmail.com>
2024-05-13 16:54:55 +0600

    implement Environment Variables to hide the Firebase config keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJoMMU0mzKmXmXsGbyDQnRATCUcnm3xJo",
-  authDomain: "tastematrix.firebaseapp.com",
-  projectId: "tastematrix",
-  storageBucket: "tastematrix.appspot.com",
-  messagingSenderId: "933186519403",
-  appId: "1:933186519403:web:70044070478b66d28b0c2e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
