2923ab01966039a1100da125536aad04cf48bcab
HprogrammerArif <programmerhabluarif@gmail.com>
2024-05-23 00:04:56 +0600

    added filter and search functionality

--- src/pages/AllFoods/AllFoods.jsx
+++ src/pages/AllFoods/AllFoods.jsx
@@ -7,7 +7,10 @@ const AllFoods = () => {
   const [itemsPerPage, setItemsPerPage] = useState(4);
   const [currentPage, setCurrentPage] = useState(1);
   const [counts, setCount] = useState();
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState('')
+  const [search, setSearch] = useState([])
+  const [searchText, setSearchText] = useState('')
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
@@ -15,19 +18,20 @@ const AllFoods = () => {
       const { data } = await axios(
         `${
           import.meta.env.VITE_API_URL
-        }/all-foods?page=${currentPage}&size=${itemsPerPage}&filter=${filter}`
+        }/all-foods?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
       );
       setFoods(data);
     };
     getData();
-  }, [itemsPerPage, currentPage, filter]);
-  console.log(filter);
+  }, [itemsPerPage, currentPage, filter, sort, search]);
+  //console.log(filter);
+
 
   useEffect(() => {
     const getCount = async () => {
       try {
         const { data } = await axios(
-          `${import.meta.env.VITE_API_URL}/foods-count`
+          `${import.meta.env.VITE_API_URL}/foods-count?filter=${filter}&search=${search}`
         );
         setCount(data.count);
       } catch (error) {
@@ -35,10 +39,12 @@ const AllFoods = () => {
       }
     };
     getCount();
-  }, []);
+  }, [filter, search]);
 
   //console.log(counts);
   //console.log(itemsPerPage);
+
+
 
   const numberOfPages = Math.ceil(counts / itemsPerPage);
 
@@ -46,12 +52,30 @@ const AllFoods = () => {
     (element) => element + 1
   );
 
-  // handle pagination
 
+
+  // handle pagination
   const handlePaginationButton = (value) => {
     console.log(value);
     setCurrentPage(value);
   };
+
+  //reset
+  const handleReset = () => {
+    setFilter('')
+    setSort('')
+    setSearch('')
+    setSearchText('')
+  }
+
+  const handleSearch = e => {
+    e.preventDefault();
+    // const text = e.target.search.value;
+    // setSearch(text)
+    setSearch(searchText)
+  }
+
+  console.log(search);
 
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -59,8 +83,11 @@ const AllFoods = () => {
         <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
           <div>
             <select
-            onChange={(e) => setFilter(e.target.value)}
-            value={filter}
+              onChange={(e) => {
+                setFilter(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={filter}
               name="category"
               id="category"
               className="border p-4 rounded-lg"
@@ -73,9 +100,11 @@ const AllFoods = () => {
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
               <input
+              onChange={(e) => setSearchText(e.target.value)}
+              value={searchText}
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
                 name="search"
@@ -90,16 +119,21 @@ const AllFoods = () => {
           </form>
           <div>
             <select
-              name="category"
-              id="category"
+             onChange={(e) => {
+              setSort(e.target.value)
+              setCurrentPage(1)
+            }}
+            value={sort}
+              name="sort"
+              id="sort"
               className="border p-4 rounded-md"
             >
-              <option value="">Sort By Deadline</option>
-              <option value="dsc">Descending Order</option>
-              <option value="asc">Ascending Order</option>
+              <option value="">Sort By Cost</option>
+              <option value="dsc">High Price</option>
+              <option value="asc">Low Price</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">Reset</button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-12 md:grid-cols-2 lg:grid-cols-3 ">
           {foods.map((food) => (
@@ -112,10 +146,11 @@ const AllFoods = () => {
       <div className="flex justify-center mt-12">
         {/* previous button */}
 
-        <button 
-        onClick={() => handlePaginationButton(currentPage-1)}
-        disabled={currentPage === 1}
-         className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
+        <button
+          onClick={() => handlePaginationButton(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
+        >
           <div className="flex items-center -mx-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -151,9 +186,10 @@ const AllFoods = () => {
 
         {/* next button */}
         <button
-        onClick={() => handlePaginationButton(currentPage+1)}
-        disabled={currentPage === numberOfPages}
-         className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
+          onClick={() => handlePaginationButton(currentPage + 1)}
+          disabled={currentPage === numberOfPages}
+          className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+        >
           <div className="flex items-center -mx-1">
             <span className="mx-1">Next</span>
 
