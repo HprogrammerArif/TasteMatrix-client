ae748636128003c49f6df3f331b5d3c06e742a2a
HprogrammerArif <programmerhabluarif@gmail.com>
2024-05-22 22:25:16 +0600

    added next and previous button functionality

--- src/pages/AllFoods/AllFoods.jsx
+++ src/pages/AllFoods/AllFoods.jsx
@@ -5,45 +5,51 @@ import TopFoodCard from "../../components/TopFoods/TopFoodCard";
 
 const AllFoods = () => {
   const [itemsPerPage, setItemsPerPage] = useState(2);
-  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
   const [counts, setCount] = useState();
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios(`${import.meta.env.VITE_API_URL}/all-foods?page=${currentPage}&size=${itemsPerPage}`);
+      const { data } = await axios(
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-foods?page=${currentPage}&size=${itemsPerPage}`
+      );
       setFoods(data);
     };
     getData();
   }, [itemsPerPage, currentPage]);
 
- 
   useEffect(() => {
     const getCount = async () => {
       try {
-        const { data } = await axios(`${import.meta.env.VITE_API_URL}/foods-count`);
+        const { data } = await axios(
+          `${import.meta.env.VITE_API_URL}/foods-count`
+        );
         setCount(data.count);
       } catch (error) {
-        console.error('Error fetching food count:', error);
+        console.error("Error fetching food count:", error);
       }
     };
     getCount();
   }, []);
-  
 
- //console.log(counts);
- //console.log(itemsPerPage);
+  //console.log(counts);
+  //console.log(itemsPerPage);
 
-const numberOfPages = Math.ceil(counts / itemsPerPage);
+  const numberOfPages = Math.ceil(counts / itemsPerPage);
 
-  const pages = [...Array(numberOfPages || 0).keys()].map(element => element + 1);
+  const pages = [...Array(numberOfPages || 0).keys()].map(
+    (element) => element + 1
+  );
 
   // handle pagination
 
   const handlePaginationButton = (value) => {
     console.log(value);
-    setCurrentPage(value)
-  }
+    setCurrentPage(value);
+  };
 
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -101,7 +107,10 @@ const numberOfPages = Math.ceil(counts / itemsPerPage);
       <div className="flex justify-center mt-12">
         {/* previous button */}
 
-        <button className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
+        <button 
+        onClick={() => handlePaginationButton(currentPage-1)}
+        disabled={currentPage === 1}
+         className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
           <div className="flex items-center -mx-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -125,16 +134,21 @@ const numberOfPages = Math.ceil(counts / itemsPerPage);
         {/* number  */}
         {pages.map((btnNum) => (
           <button
-          onClick={() => handlePaginationButton(btnNum)}
+            onClick={() => handlePaginationButton(btnNum)}
             key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            className={`hidden ${
+              currentPage === btnNum ? "bg-blue-500 text-white" : ""
+            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
           >
             {btnNum}
           </button>
         ))}
 
         {/* next button */}
-        <button className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
+        <button
+        onClick={() => handlePaginationButton(currentPage+1)}
+        disabled={currentPage === numberOfPages}
+         className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
           <div className="flex items-center -mx-1">
             <span className="mx-1">Next</span>
 
